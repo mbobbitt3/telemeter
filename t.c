9ccf4954c55919d3b3bfa0394dc2ce99c132c355
Barry Rountree <barry.rountree@protonmail.com>
2021-06-19 05:03:03 +0000

    Stopping for the evening.

--- t.c
+++ t.c
@@ -11,18 +11,63 @@
 #include <inttypes.h>		// PRIx8 and friends
 #include <errno.h>		// definition of errno
 #include <stdlib.h>		// exit()
+#include "../msr-safe/msr_safe.h"
 
-#define ONE_GIGABYTE (1024ULL*1024*1024*1)
 
-int main(){
-	fprintf( stdout, "gethugepagesize() returns %ld.\n", gethugepagesize() );
-	uint8_t *op = get_huge_pages( 2 * ONE_GIGABYTE, GHP_DEFAULT );
-	if( NULL == op ){
-		fprintf(stderr, "%s:%d get_huge_pages failed (errno=%d)",
+#define ONE_GiB (1024ULL*1024*1024)
+
+void*
+allocate_GiB_pages( size_t n_GiB ){
+	long sz = gethugepagesize();
+	if( -1 == sz ){
+		fprintf(stderr, "%s:%d gethugepagesize() failed (errno=%d)",
 			       	__FILE__, __LINE__, errno); 
 		perror("perror reports:  ");
 		exit(-1);
 	}
-	free_huge_pages(op);
+	if( sz != ONE_GiB ){
+		fprintf(stderr, "%s:%d gethugepagesize() reports the default huge page size is %ld.\n", __FILE__, __LINE__, sz);
+		fprintf(stderr, "We only support %ld huge pages at the moment.", ONE_GiB );
+		exit(-1);
+	}
+
+	uint8_t *op = get_huge_pages( n_GiB * ONE_GIGABYTE, GHP_DEFAULT );
+	if( NULL == op ){
+		fprintf(stderr, "%s:%d get_huge_pages() failed (errno=%d)",
+			       	__FILE__, __LINE__, errno); 
+		perror("perror reports:  ");
+		exit(-1);
+	}
+	return op;
+}
+
+void
+free_GiB_pages( void *p ){
+	free_huge_pages( p );
+}
+
+void
+init_poll_energy( struct msr_batch_array *a ){
+
+}
+
+void
+poll_energy(){
+
+}
+
+void
+finalize_poll_energy(){
+
+}
+
+void
+dump_poll_energy(){
+
+}
+
+
+int main(){
+
 	return 0;
 }
